96c18fd0085a3f46593025863a8687ba2119eeb0
Astrid Hecht <silentaviary@gmail.com>
2022-06-09 14:31:42 -0600

    Add less than 60

--- friends.rb
+++ friends.rb
@@ -1,6 +1,12 @@
 numbers = [2, 7, 53, 27, 1, 7, 99, 87]
 numbers.each do |number|
   if number > 30
-    puts number
+    puts "#{number} is greater than 30."
+  end
+end
+
+numbers.each do |number|
+  if number < 60
+    puts "#{number} is less than 60."
   end
 end
